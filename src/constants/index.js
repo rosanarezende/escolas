3d21292bd2463afa03d87dec134330d396aa570a
rosanarezende <rezende_rosana@hotmail.com>
2023-09-28 02:24:44 -0300

    altera categorias

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
 export const CATEGORIAS = [
   { id: 1, nome: "Ação" },
-  { id: 2, nome: "Aventura" },
-  { id: 3, nome: "Corrida" },
-  { id: 4, nome: "Esporte" },
-  { id: 5, nome: "Estratégia" },
-  { id: 6, nome: "RPG" },
+  { id: 2, nome: "Estrategia" },
+  { id: 3, nome: "Aventura" },
+  { id: 4, nome: "RPG" },
+  { id: 5, nome: "Esporte" },
+  { id: 6, nome: "Corrida" },
 ];
