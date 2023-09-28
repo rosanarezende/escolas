e2964ac834746778b720655e22a4e5ecd922b88a
rosanarezende <rezende_rosana@hotmail.com>
2023-09-28 04:07:33 -0300

    remove imports desnecessários

--- sem-integracao/src/main.jsx
+++ sem-integracao/src/main.jsx
@@ -1,13 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { JogosProvider } from "./contexts/jogosContext.jsx";
-import { EstudiosProvider } from "./contexts/estudiosContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <JogosProvider>
-    <EstudiosProvider>
-      <App />
-    </EstudiosProvider>
-  </JogosProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
